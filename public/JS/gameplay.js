fc257910522080673d2d26188dfbbd623870f246
Nattawut-artn <natthawutartnonladev@gmail.com>
2025-12-05 15:47:09 +0700

    feat : update comment read and write popup

--- public/JS/gameplay.js
+++ public/JS/gameplay.js
@@ -3,6 +3,8 @@ window.onload = init;
 function init(){
     let hear = document.getElementById('hear')
     hear.onclick = pop;
+
+    initcommentpopup()
 }
 
 function pop(){
@@ -12,4 +14,26 @@ function pop(){
     setTimeout(() => {
         hear.src = "CSS/Pictures/Backgrounds/here2.png";
     }, 250);
+}
+
+function initcommentpopup(){
+    let popups = document.querySelectorAll('.b')
+    for(i = 0 ; i < popups.length ; i++){
+        let index = i
+        popups[i].onclick = () => showpopup(index)
+    }
+}
+
+function showpopup(index){
+    let popup = document.getElementById('comment-popup')
+    popup.style.display = 'flex'
+
+    let cancel = document.getElementById('cancel')
+    cancel.addEventListener('click',() =>{
+        popup.style.display = 'none'
+    })
+    let submit = document.getElementById('submit')
+    submit.addEventListener('click',() =>{
+        popup.style.display = 'none'
+    })
 }
